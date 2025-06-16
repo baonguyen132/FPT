8823e53340c5b9e602e363a168539eb0316a0f3c
Bảo Nguyên <118587919+baonguyen132@users.noreply.github.com>
2025-06-16 15:30:08 +0700

    fix button and create input

---
+++ frontend/src/Input.js
@@ -0,0 +1,10 @@
+import './Button.css';
+
+function CreateButton() {
+    return (
+       <input type='text' placeholder='Username' />
+    )
+
+}
+
+export default CreateButton
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import CreateButton from './Button';
+import CreateInput from './Input';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CreateInput />
     <CreateButton />
   </React.StrictMode>
 );
