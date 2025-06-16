3f6129346d755ec0df26abb3e009167cdb59dc21
Bảo Nguyên <118587919+baonguyen132@users.noreply.github.com>
2025-06-16 15:22:15 +0700

    fix button

--- frontend/src/Button.js
+++ frontend/src/Button.js
@@ -3,9 +3,9 @@ import './Button.css';
 function CreateButton() {
     return (
         <div className="button">
-            <a className="text">
-                Click
-            </a>
+            <div className='sun-moon'>
+
+            </div>
         </div>
     )
 
