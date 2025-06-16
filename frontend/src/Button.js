e2632e58bc424c599dee5643c38e40b513d7786f
Bảo Nguyên <118587919+baonguyen132@users.noreply.github.com>
2025-06-16 14:36:21 +0700

    feat: creare button

---
+++ frontend/src/Button.js
@@ -0,0 +1,14 @@
+import './Button.css';
+
+function CreateButton() {
+    return (
+        <div className="button">
+            <a className="text">
+                Click
+            </a>
+        </div>
+    )
+
+}
+
+export default CreateButton
